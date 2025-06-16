f8539b00ebba5c1be6b7921ed633e83324316418
Olaiwonismail <olaiwonismail@gmail.com>
2025-06-16 10:34:23 +0100

    commit

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,16 +1,17 @@
-from flask import render_template, url_for, flash, redirect, request, Blueprint
-from flask_login import login_user, current_user, logout_user, login_required
-from app import db, bcrypt
-from app.models import User
-# 
-# from app.auth.utils import save_pic, send_reset_email
-auth= Blueprint('auth',__name__)
 
-@auth.route('/account',methods = ['POST','GET'])
-def account():
-    pass 
-@auth.route("/user/<username>")
-def user_posts(username):
-    return username
+from flask import Blueprint
+
+auth = Blueprint('auth',__name__)
 
 
+@auth.route('/login')
+def login():
+    return "Login Page"
+
+@auth.route('/logout')  
+def logout():
+    return "Logout Page"
+
+@auth.route('/register')
+def register():
+    return "Register Page"

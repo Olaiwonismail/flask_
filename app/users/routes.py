7876d025cfa3a325c652d7821220786e507ddb08
Olaiwonismail <olaiwonismail@gmail.com>
2025-06-16 10:34:36 +0100

    commit

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,16 +1,16 @@
-from flask import Blueprint
+from flask import render_template, url_for, flash, redirect, request, Blueprint
+from flask_login import login_user, current_user, logout_user, login_required
+from app import db, bcrypt
+from app.models import User
+# 
+# from app.auth.utils import save_pic, send_reset_email
+users= Blueprint('users',__name__)
 
-users = Blueprint('users',__name__)
+@users.route('/account',methods = ['POST','GET'])
+def account():
+    pass 
+@users.route("/user/<username>")
+def user_posts(username):
+    return username
 
 
-@users.route('/login')
-def login():
-    return "Login Page"
-
-@users.route('/logout')  
-def logout():
-    return "Logout Page"
-
-@users.route('/register')
-def register():
-    return "Register Page"
